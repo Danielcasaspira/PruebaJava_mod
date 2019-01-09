790f6f6dc44de5492bb1922f4e486c7e5f853e26
Danielcasaspira <46467018+Danielcasaspira@users.noreply.github.com>
2019-01-09 11:32:35 -0500

    ORACLE sql developer
    ORACLE sql developer

---
+++ Usuarios1.sql
@@ -0,0 +1,9 @@
+Create table USUARIOS (nombre varchar2(30),apellidopaterno varchar2(30),apellidomaterno varchar2(30),email varchar2(50), telefono varchar2(20));
+
+
+describe USUARIOS;
+
+Insert into USUARIOS values('daniel','casas','pira','hdaniel0519@hotmail.com','3192948662');
+Insert into USUARIOS values('nicole','casas','pira','niclepira@hotmail.com','3192546721');
+
+select * from USUARIOS;
